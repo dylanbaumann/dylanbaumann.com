89fe1b4062c514e38177e1abdc97e6fe4d66e503
Dylan Baumann <dylanbaumann@Dylans-MacBook-Pro.local>
2017-02-09 11:40:00 -0600

    Beginning typographic groundwork

---
+++ page-typography.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+<main>
+	<article id="page-typography" <?php post_class(); ?>>
+
+		<section>
+			<h1>Opening h1 with a long title because that's a thing that happens</h1>
+
+			<p>Separated by a block of paragraph copy to demonstrate margin-bottom settings.</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit <a>anim id est laborum</a>.</p>
+
+			<h2>Lists</h2>
+			<p>How both Ordered and Unordered lists behave.</p>
+
+			<ul>
+				<li>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+				<li>Aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+				<li>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+				<li>Quis nostrud et all.</li>
+				<li>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+			</ul>
+
+			<p>Separated by a block of paragraph copy to demonstrate margin-bottom and margin-top settings.</p>
+
+			<ol>
+				<li>First do this</li>
+				<li>Then do that thing next</li>
+				<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</li>
+				<li>followed by a short thing</li>
+			</ol>
+
+			<p>Separated by a block of paragraph copy to demonstrate margin-bottom and margin-top settings.</p>
+		</section>
+
+	</article>
+</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
